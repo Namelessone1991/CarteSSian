740f75c1a15d0e86fad09c366dea29d6731fec1d
nameless <Ernesto.Roldan.Rh@gmail.com>
2017-06-13 13:18:58 -0500

    Completed basic function

--- CarteSSian.ts
+++ CarteSSian.ts
@@ -1,19 +1,6 @@
 $(document).ready( function()
 {
-
-  console.log('This is a test'); 
-
-
-  let cssParam: string = "<style> body { 'position':'absolute', 'color':'blue', 'font-family': '\"Serif\", \"Times new Roman\"', 'font-size':'20px', 'left':'100px','top':'100px'} </style>";
-   
-
-  $(cssParam).appendTo("Head");
-
-
-//  $(".test").data("stylesData", {color:'blue'});
-
-let value = 'yellow';
-let back = 'green';
+ 
 
 let list: any[] = [{'background-color':'black'}];
 
@@ -24,10 +11,6 @@ list.push({'color':'blue'});
 
 let body = 'body';
 
-
-
-
-//console.log("Key values"+Object.keys(list).length);
 
 function paramTest(element: string, x: number, y: number, cssParams: any[])
    {
@@ -45,37 +28,6 @@ function paramTest(element: string, x: number, y: number, cssParams: any[])
 
 paramTest(body,0,0,list);
 
-
-
- // paramTest(0,100, cssParam);
-    // paramTest(0,100,"\'position\':\'absolute\', \'color\':\'blue\'");
-
-
-   
-    
-    
-
-
-     //console.log("The values are  x  "+x+" y  "+y+ " "+ cssParams.join(" ") );
-
-
-  
-
-  /*
-
-  function setCss(x: number, y: number,  ) //this uses coordinates based on the 
-  {
-    
-
-      
-  }
-
-*/
-
-
-  // $(".test").css({ 'position':'absolute', 'color':'blue', 'font-family': '\"Serif\", \"Times new Roman\"', 'font-size':'20px', 'left':'100px','top':'100px'});
-   
-   // $("body").css({'background-color':'black', 'color':'red'});
 
 
 
