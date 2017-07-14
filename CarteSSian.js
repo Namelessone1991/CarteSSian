4e8e74af7f389db6d7c00dece10cf3a1d798baaf
nameless <Ernesto.Roldan.Rh@gmail.com>
2017-07-14 15:47:33 -0500

    Latest changes reformating of classes

--- CarteSSian.js
+++ CarteSSian.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
             this.width = width;
         }
         cartessian.prototype.addCss = function (cssvalues) {
+            //this is the function that will be used to sort the array entries by alphabetical order
             cssvalues.sort(compare);
             for (var i = 0; Object.keys(cssvalues).length < i; i++) {
                 cssvalues[i].id = i;
@@ -24,15 +25,80 @@ $(document).ready(function () {
         };
         return cartessian;
     }());
+    function Search(searchData, searchValue) {
+        var index = searchData.length - 1;
+        var half = Math.floor(index / 2);
+        console.log("Value of the half" + half);
+        while (half <= index || half >= 0) {
+            if (searchData[half].property === searchValue) {
+                return half;
+            }
+            if (searchData[half].property > searchValue) {
+                half--;
+                if ((searchData[half].property !== searchValue) && (half <= 0)) {
+                    return -1;
+                }
+                if ((searchData[half].property === searchValue) && (half >= 0)) {
+                    return half;
+                }
+            }
+            if (searchData[half].property < searchValue) {
+                half++;
+                if ((searchData[half].property !== searchValue) && (half >= searchData.length - 1)) {
+                    return -1;
+                }
+                if (searchData[half].property === searchValue) {
+                    return half;
+                }
+            }
+        }
+        return -1;
+    }
+    function replaceElement(arraySource, elementToReplace) {
+        for (var i = 0; i < elementToReplace.length; i++) {
+            var exists = Search(arraySource, elementToReplace[i].property);
+            console.log("value of exists :" + exists);
+            if (exists !== -1) {
+                console.log("The value of exists is: " + exists);
+                arraySource[exists] = elementToReplace[i];
+                console.log("The value was replaced into the string" + JSON.stringify(elementToReplace[i]));
+            }
+            if (exists === -1) {
+                arraySource.push(elementToReplace[i]);
+                arraySource.sort(compare);
+            }
+        }
+    }
     //this is the format for all the arrays, this will allow 
     //one to search through indexes
     var cssData = [
-        { id: 67, property: "color", value: "red" },
-        { id: 56, property: "background-color", value: "black" }
+        { id: 0, property: "background-color", value: "black" },
+        { id: 1, property: "color", value: "white" },
+        { id: 2, property: "delta", value: "delta" }
     ];
-    //this is the function that will be used to sort the array entries by alphabetical order
-    //cssData.sort(compare);
+    var replacer = [
+        { id: 0, property: "color", value: "red" },
+        { id: 1, property: "background-color", value: "black" },
+        { id: 2, property: "red", value: "Times" },
+        { id: 3, property: "miko", value: "Times" }
+    ];
+    console.log("Value of searching " + Search(cssData, "hjshjshjs"));
+    replaceElement(cssData, replacer);
+    /*
+     console.log("Element before the change property : "+cssData[1].property + " value: "+cssData[1].value);
+      
+      
+    
+    
+     console.log("Element AFTER the change property : "+cssData[1].property + " value: "+cssData[1].value);
+    
+     console.log("Element AFTER the change property background color : "+cssData[0].property + " value: "+cssData[0].value);
+    
+    console.log("The result of applying the array search in cssData is:  property " + cssData[Search(cssData, "color")].property+ " The value is "+ cssData[Search(cssData, "color")].value);
+    
+    */
     $("body").css(cssData[0].property, cssData[0].value);
     $("body").css(cssData[1].property, cssData[1].value);
+    // $("body").css(cssData[2].property, cssData[2].value);
     //$("body").css({cssData[0].property, "'red'"});
 });
--- CarteSSian.ts
+++ CarteSSian.ts
@@ -23,6 +23,10 @@ $(document).ready(function () {
 
     properties: any[];
 
+    replaceElement: (arraySource: any[], elementToReplace: any[]) => void;
+
+    
+
     constructor(element: string, x: number, y: number, height?: number, width?: number) {
 
       this.element = element;
@@ -37,11 +41,13 @@ $(document).ready(function () {
     }
 
     addCss(cssvalues: any[]) {
-      
+
+
+      //this is the function that will be used to sort the array entries by alphabetical order
       cssvalues.sort(compare);
 
       for (let i = 0; Object.keys(cssvalues).length < i; i++) {
-      
+
         cssvalues[i].id = i;
 
         this.properties.push(cssvalues[i]);
@@ -54,27 +60,140 @@ $(document).ready(function () {
   }
 
 
+  function Search(searchData: any[], searchValue: string) {
+
+    let index = searchData.length - 1;
+
+    let half = Math.floor(index / 2);
+
+
+    console.log("Value of the half" + half);
+
+    while (half <= index || half >= 0) {
+
+      if (searchData[half].property === searchValue) {
+
+        return half;
+      }
+
+      if (searchData[half].property > searchValue) {
+
+        half--;
+
+        if ((searchData[half].property !== searchValue) && (half <= 0)) {
+
+          return -1;
+        }
+
+
+        if ((searchData[half].property === searchValue) && (half >= 0)) {
+
+
+
+          return half;
+        }
+
+      }
+
+      if (searchData[half].property < searchValue) {
+        half++;
+
+        if ((searchData[half].property !== searchValue) && (half >= searchData.length - 1)) {
+
+          return -1;
+        }
+
+
+        if (searchData[half].property === searchValue) {
+          return half;
+        }
+      }
+
+    }
+
+    return -1;
+  }
+
+
+
+
+
+
+   cartessian.prototype.replaceElement = (arraySource: any[], elementToReplace: any[]): void => {
+
+    for (let i = 0; i < elementToReplace.length; i++) {
+      let exists = Search(arraySource, elementToReplace[i].property);
+
+      console.log("value of exists :" + exists);
+      if (exists !== -1) {
+
+        console.log("The value of exists is: " + exists);
+
+        arraySource[exists] = elementToReplace[i];
+
+        console.log("The value was replaced into the string" + JSON.stringify(elementToReplace[i]));
+
+      }
+
+      if (exists === -1) {
+
+        
+
+        arraySource.push(elementToReplace[i]);
+
+        arraySource.sort(compare);
+
+      }
+
+
+
+    }
+
+  }
+
+  
+ 
+
+
+  let a = "something";
+   cartessian[a]();
+
+   
+
+
+
+
+
+
 
   //this is the format for all the arrays, this will allow 
   //one to search through indexes
   var cssData: { id: number, property: string, value: string }[] = [
-    { id: 67, property: "color", value: "red" },
-    { id: 56, property: "background-color", value: "black" }
+    { id: 0, property: "background-color", value: "black" },
+    { id: 1, property: "color", value: "white" },
+    { id: 2, property: "delta", value: "delta" }
   ];
 
 
 
-  //this is the function that will be used to sort the array entries by alphabetical order
-  //cssData.sort(compare);
+  var replacer: { id: number, property: string, value: string }[] = [
+    { id: 0, property: "color", value: "red" },
+    { id: 1, property: "background-color", value: "black" },
+    { id: 2, property: "red", value: "Times" },
+    { id: 3, property: "miko", value: "Times" }
+  ];
 
 
 
+  console.log("Value of searching " + Search(cssData, "hjshjshjs"));
 
+   
 
 
 
   $("body").css(cssData[0].property, cssData[0].value);
   $("body").css(cssData[1].property, cssData[1].value);
+  // $("body").css(cssData[2].property, cssData[2].value);
   //$("body").css({cssData[0].property, "'red'"});
 
 
